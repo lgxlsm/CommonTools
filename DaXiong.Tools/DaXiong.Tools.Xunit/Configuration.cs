219eff531dc06e4fccb7f5ee5075380d12db2ff1
lgxlsm <lgxlsm@163.com>
2023-05-21 22:56:42 +0800

    添加测试模块

---
+++ DaXiong.Tools/DaXiong.Tools.Xunit/Configuration.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace DaXiong.Tools.Xunit
+{
+    public class Configuration
+    {
+        public Configuration()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
