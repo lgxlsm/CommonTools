513f399f56d68987615b872c84487e7bd2e5558c
lgxlsm <lgxlsm@163.com>
2023-05-26 00:24:28 +0800

    TimeMonitor

---
+++ DaXiong.Tools/DaXiong.Tools.Middleware/Monitor/TimeMonitorExtension.cs
@@ -0,0 +1,37 @@
+﻿using DaXiong.Tools.Middleware.Monitor;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class TimeMonitorExtension
+    {
+        public static IServiceCollection AddTimeMonitor(this IServiceCollection services, Action<TimeMonitorOption> configure = null)
+        {
+            //services.AddOptions<TimeMonitorOption>();
+
+            //services.PostConfigure<TimeMonitorOption>(x =>
+            //{
+            //    configure?.Invoke(x);
+            //});
+
+            //services.AddBuffering();
+
+            return services.AddSingleton<IStartupFilter, TimeMonitorStartupFilter>();
+        }
+    }
+
+    internal class TimeMonitorStartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                app.UseMiddleware<TimeMonitorMiddleware>();
+                next(app);
+            };
+        }
+    }
+}
